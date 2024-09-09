407ed66ab1453549e4e9edfc4c607737d76f4067
Desiree Ann Guzman <desireeann000@gmail.com>
2024-09-09 08:11:19 +0800

    Completed Variables Section

---
+++ variables.js
@@ -0,0 +1,10 @@
+console.log("****VARIABLES****");
+let name = "Desiree Ann P. Guzman"; 
+let whatDoYouWannaBecomeInYourLife = "Programmer";
+let gender = "Female";
+let twitterHandle = "@DesiNeverQuits"; 
+
+console.log("Name:", name);
+console.log("What do you want to become in your life?", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
